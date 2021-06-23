4d22e27e14b156ee51b5bcc0704c3b00a8193b08
zhouq <15983087710@163.com>
2021-06-23 15:15:14 +0800

    first commit ,only frame

---
+++ ark_pro/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <div class="msg">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.hello {
+  .msg {
+    color: red;
+  }
+}
+</style>
